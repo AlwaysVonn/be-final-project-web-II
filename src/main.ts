4ca57df8f42048fe93d6ca21647905584718a8d0
roldleo <haroldleopanja@gmail.com>
2025-05-03 02:27:18 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ async function bootstrap() {
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   await app.init();
+  app.enableCors({
+    origin: 'https://be-self-management-git-main-roldleos-projects.vercel.app', // URL aplikasi Vercel kamu
+  });
   server.listen(3000, () => console.log('Server running...'));
 }
 bootstrap();

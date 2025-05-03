960d6b0cb1b4a40737eaed46c44cb4e18939b3d4
roldleo <haroldleopanja@gmail.com>
2025-05-03 08:36:21 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   await app.init();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://self-management-xi.vercel.app/'],
+    credentials: true,
+  });
   server.listen(3000, () => console.log('Server running...'));
 }
 bootstrap();
